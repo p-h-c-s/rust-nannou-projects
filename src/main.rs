70dbe2ebc5f330aed145722c9c4ed3e9d98aa9a9
p-h-c-s <pedroceravolo@jusbrasil.com.br>
2024-07-27 15:44:06 -0400

    original

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use nannou::prelude::*;
 use nannou::image::{DynamicImage, GenericImageView, RgbImage, Rgba};
 use nannou::color::DARKBLUE;
+use num::Complex;
+
+pub mod mandelbrot;
 
 fn main() {
     nannou::app(model).run();
@@ -12,8 +15,8 @@ struct Model {
 }
 
 fn model(app: &App) -> Model {
-    let window = app.new_window().view(view).build().unwrap();
-    let mut image = DynamicImage::new_rgb8(100, 100); // Adjust size as needed
+    let window = app.new_window().size(1000, 1000).view(view).build().unwrap();
+    let mut image = DynamicImage::new_rgb8(1600, 1600); // Adjust size as needed
     render(app, &mut image);
     Model { window, image }
 }
@@ -24,20 +27,26 @@ fn render(app: &App, image: &mut DynamicImage) {
     let height = image.height() as f32;
 
     for (x, y, pixel) in image.as_mut_rgb8().unwrap().enumerate_pixels_mut() {
-        let fx = map_range(x as f32, 0.0, width, win.left(), win.right());
-        let fy = map_range(y as f32, 0.0, height, win.bottom(), win.top());
+        // values taken from wiki: https://en.wikipedia.org/wiki/Mandelbrot_set
+        let fx: f32 = map_range(x as f32, 0.0, width, -2.00, 0.47);
+        let fy: f32 = map_range(y as f32, 0.0, height, -1.12, 1.12);
         let color = color_function(fx, fy);
         *pixel = nannou::image::Rgb([color.0[0], color.0[1], color.0[2]]);
     }
 }
 
 fn color_function(x: f32, y: f32) -> Rgba<u8> {
-    // Replace this with your custom color function
-    // Example: create a gradient based on x and y
-    let r = (map_range(x, -300.0, 300.0, 0.0, 255.0) as u8);
-    let g = (map_range(y, -300.0, 300.0, 0.0, 255.0) as u8);
-    let b = 128;
-    Rgba([r, g, b, 255])
+    match mandelbrot::is_in_set(Complex::new(x, y)) {
+        true => {
+            return Rgba([0, 0, 0, 255]);
+        }
+        false => {
+            let r = (map_range(x, -300.0, 300.0, 0.0, 255.0) as u8);
+            let g = (map_range(y, -300.0, 300.0, 0.0, 255.0) as u8);
+            let b = 128;
+            return Rgba([r, g, b, 255]);
+        }
+    }
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
@@ -48,6 +57,7 @@ fn view(app: &App, model: &Model, frame: Frame) {
     draw.texture(&texture)
         .w_h(app.window_rect().w(), app.window_rect().h());
     // draw.texture(&texture);
+    // draw.texture(&texture).x_y(0.0, 0.0);
 
     draw.to_frame(app, &frame).unwrap();
 }
