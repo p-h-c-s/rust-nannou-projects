78a53306430862f02f51d7184cf350d7db44c750
p-h-c-s <pedroceravolo@jusbrasil.com.br>
2024-07-27 17:51:57 -0400

    primitive zoom

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use nannou::color::DARKBLUE;
 use nannou::image::{DynamicImage, GenericImageView, RgbImage, Rgba};
 use nannou::prelude::*;
+use nannou::event::Event;
+use nannou::winit::event::MouseScrollDelta;
 use num::Complex;
 
 pub mod mandelbrot;
 
 fn main() {
-    nannou::app(model).run();
+    nannou::app(model).event(event).run();
 }
 
 // boundaries of the complex plane so the set is nicely visible. Taken from https://en.wikipedia.org/wiki/Mandelbrot_set
@@ -18,28 +20,92 @@ const MAX_Y: f32 = 1.12;
 struct Model {
     window: WindowId,
     image: DynamicImage,
+    zoom: f64
 }
 
 fn model(app: &App) -> Model {
-    app.set_loop_mode(LoopMode::wait());
+    // nannou continuously tries to draw the texture from the rendered image by calling "view" for each frame
+    // Wait mode only redraws when an event happens
+    app.set_loop_mode(LoopMode::wait()); 
     let window = app
         .new_window()
         .size(1000, 1000)
         .view(view)
         .build()
         .unwrap();
-    let mut image = DynamicImage::new_rgb8(1000, 1000); // Adjust size as needed
-    render(app, &mut image);
-    Model { window, image }
+    let image = DynamicImage::new_rgb8(1000, 1000); // Adjust size as needed
+    let mut model = Model { window, image, zoom: 1.5};
+    render(app, &mut model);
+    model
 }
 
-fn render(app: &App, image: &mut DynamicImage) {
-    let width = image.width() as f32;
-    let height = image.height() as f32;
+fn event(app: &App, model: &mut Model, event: Event) {
+    match event {
+        Event::WindowEvent { id: _, simple: Some(w_event) } => {
+            if let WindowEvent::MouseWheel ( delta, phase, .. ) = w_event {
+                if let Some(zoom) = handle_zoom(delta, phase) {
+                    println!("{:?}", zoom);
+                    model.zoom = zoom;
+                }
+                render(app, model);
+            }
+        }
+        _ => {}
+    }
+}
 
-    for (x, y, pixel) in image.as_mut_rgb8().unwrap().enumerate_pixels_mut() {
-        let fx: f32 = map_range(x as f32, 0.0, width, MIN_X, MAX_X);
-        let fy: f32 = map_range(y as f32, 0.0, height, MIN_Y, MAX_Y);
+fn handle_zoom(delta: MouseScrollDelta, phase: TouchPhase) -> Option<f64> {
+    // maybe only calculate if the phase is correct
+    let zoom_factor = match delta {
+        MouseScrollDelta::LineDelta(_, y) => {
+            if y > 0.0 { 1.1 } else { 0.9 }
+        }
+        MouseScrollDelta::PixelDelta(pos) => {
+            if pos.y > 0.0 { 1.1 } else { 0.9 }
+        }
+    };
+
+    match phase {
+        TouchPhase::Moved => {
+            Some(zoom_factor)
+        }
+        _ => {
+            // Scroll gesture was interrupted
+            None
+        }
+    }
+
+    // for ref:
+    // match phase {
+    //     TouchPhase::Started => {
+    //         // Scroll gesture has started
+    //     }
+    //     TouchPhase::Moved => {
+    //         // Scroll is in progress
+    //         model.zoom *= zoom_factor;
+    //     }
+    //     TouchPhase::Ended => {
+    //         // Scroll gesture has ended
+    //     }
+    //     TouchPhase::Cancelled => {
+    //         // Scroll gesture was interrupted
+    //     }
+    // }
+
+
+    // Flag that we need to redraw
+}
+
+
+
+fn render(app: &App, model: &mut Model) {
+    let width = model.image.width() as f32;
+    let height = model.image.height() as f32;
+    let z = model.zoom as f32;
+
+    for (x, y, pixel) in model.image.as_mut_rgb8().unwrap().enumerate_pixels_mut() {
+        let fx: f32 = map_range(x as f32, 0.0, width, MIN_X, MAX_X) / z;
+        let fy: f32 = map_range(y as f32, 0.0, height, MIN_Y, MAX_Y) / z;
         let color = color_function(fx, fy);
         *pixel = nannou::image::Rgb([color.0[0], color.0[1], color.0[2]]);
     }
@@ -62,6 +128,7 @@ fn color_function(x: f32, y: f32) -> Rgba<u8> {
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(DARKBLUE);
+    // render(app, model);
 
     let texture = wgpu::Texture::from_image(app, &model.image);
     draw.texture(&texture)
