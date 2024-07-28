794f44d115aaef07e12ba0171694850826867fa4
p-h-c-s <pedroceravolo@jusbrasil.com.br>
2024-07-27 23:10:16 -0400

    remove unused tests

--- mandelbrot/mandelbrot.rs
+++ mandelbrot/mandelbrot.rs
@@ -23,32 +23,11 @@ mod tests {
     use num::Complex;
 
     #[test]
-    fn test_mandelbrot_iteration() {
-        let start = Complex::new(0.0, 0.0);
-
-        let new_n = iterate_mandelbrot(start, Complex::new(0.0, 1.0));
-        let new_n2 = iterate_mandelbrot(new_n, Complex::new(0.0, 1.0));
-        println!("{:?}", new_n2);
-    }
-
-    #[test]
     fn test_mandelbrot_cycle_check() {
         let answer_in_set = is_in_set(Complex::new(0.0, 1.0));
         let answer_not_in_set = is_in_set(Complex::new(0.0, 2.0));
 
         assert!(matches!(answer_in_set, (true, _)));
         assert!(matches!(answer_not_in_set, (false, _)));
-    }
-
-    #[test]
-    fn test_mandelbrot_generation() {
-        let mut start_fp = 0.0;
-        let mut num = Complex::new(0.0, start_fp);
-        for _ in 1..100 {
-            let ans = is_in_set(num);
-            println!("{:?} | {:?}", num, ans);
-            start_fp += 0.1;
-            num.im = start_fp;
-        }
     }
 }
